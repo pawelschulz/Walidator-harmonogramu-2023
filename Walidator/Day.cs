6d5be4fd3a7f231be78197c7c095d49cda8f41e8
pawelschulz <73879231+pawelschulz@users.noreply.github.com>
2023-08-16 18:25:11 +0200

    działa

---
+++ Walidator/Day.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Walidator
+{
+    public class Day
+    {
+        public int month;
+        public int week;
+        public bool working;
+        public int hours;
+        public int normal_hours;
+        public int over_hours;
+        public int timebetween;
+
+        internal static List<Day> WriteDays(StreamReader sr, int n, int firstday)
+        {
+            string line = sr.ReadLine();
+            string[] table = line.Split(", ");
+
+            List<Day> listofdays = new List<Day>();
+            int i = 0;
+            while(i<=n)
+            {
+                Day daynew = new Day();
+                daynew.month = int.Parse(table[0]);
+                daynew.hours = int.Parse(table[1]);
+                daynew.week = dayofweek(firstday, i); 
+                daynew.working = (daynew.week >= 1 && daynew.week <=5) ? true : false;
+                daynew.normal_hours = (daynew.hours <= 8) ? daynew.hours : 8;
+                daynew.over_hours = (daynew.hours <= 8) ? 0 : daynew.hours-8;
+                daynew.timebetween = 24 - daynew.hours;
+                listofdays.Add(daynew);
+                i++;
+                if(i<n)
+                {
+                    line = sr.ReadLine();
+                    table = line.Split(", ");
+                }
+            }
+            return listofdays;
+        }
+
+        public static int dayofweek(int firstday, int i)
+        {
+            if (i == 0)
+                return firstday;
+            else
+            {
+                return (firstday + i) % 7;
+            }
+        }
+
+        public string whatday()
+        {
+            switch (this.week)
+            {
+                case 0: return "niedziela";
+                case 1: return "poniedziałek";
+                case 2: return "wtorek";
+                case 3: return "środa";
+                case 4: return "czwartek";
+                case 5: return "piątek";
+                case 6: return "sobota";
+                case 7: return "niedziela";
+                default: return "Nieznany dzień";
+            }
+        }
+    }
+}
---
+++ Walidator/Program.cs
@@ -0,0 +1,89 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.IO;
+using System.Globalization;
+
+namespace Walidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("To jest walidator harmonogramu czasu pracy.");
+            Console.WriteLine("Dla pierwszego dnia w miesiącu podaj jego nr porządkowy w tygodniu (pn=1, ..., nd=7):");
+            int firstday = int.Parse(Console.ReadLine());
+
+            StreamReader sr = new StreamReader("harm.in");
+            int n = new StreamReader("harm.in").ReadToEnd().Split(new char[] { '\n' }).Length;
+            List<Day> ListOfDays = Day.WriteDays(sr, n, firstday);
+            Console.WriteLine("-----------------------------------------------------------------------");
+            Console.WriteLine("Z pliku harm.in wczytałem następujące dane: ");
+            Console.WriteLine($"Liczba dni w miesiącu: {n}");
+            for (int i = 0; i < n; i++)
+            {
+                
+                Console.WriteLine($"=> dzień miesiąca: {ListOfDays[i].month}, dzień tygodnia: {ListOfDays[i].whatday()}, czy pracujący: {ListOfDays[i].working}, liczba przepracowanych godzin: {ListOfDays[i].hours}, liczba godzin: {ListOfDays[i].normal_hours}, liczba nadgodzin: {ListOfDays[i].over_hours}, czas odpoczynku: {ListOfDays[i].timebetween}.");
+            }
+            
+            Console.WriteLine("-----------------------------------------------------------------------");
+            Console.WriteLine("Walidacje: ");
+            Console.WriteLine("1) Czy suma przepracowanych godzin w miesiącu przekracza ilość [8h*liczba dni pracujących]?");
+            Console.WriteLine($"Odp: {Program.overtime_month(ListOfDays)}");
+            Console.WriteLine("2) Czy została zaplanowana praca na niedzielę?");
+            Console.WriteLine($"Odp: {Program.sunday_work(ListOfDays)}");
+            Console.WriteLine("3) Ile godzin nadgodzin?");
+            Console.WriteLine($"Odp: {Program.sum_over_hours(ListOfDays)}");
+            Console.WriteLine("4) Czy pomiędzy końcem jednego dnia, a początkiem następnego jest co najmniej 11h przerwy?");
+            Console.WriteLine($"Odp: {Program.freetime_minimum(ListOfDays)}");
+            Console.WriteLine("-----------------------------------------------------------------------");
+        }
+
+        private static object freetime_minimum(List<Day> listOfDays)
+        {
+            foreach (var day in listOfDays)
+            {
+                if (day.timebetween < 11)
+                    return "nie";
+            }
+            return "tak";
+        }
+
+        private static object sum_over_hours(List<Day> listOfDays)
+        {
+            int sumoverhours=0;
+            foreach (var day in listOfDays)
+            {
+                sumoverhours+=day.over_hours;
+            }
+            return sumoverhours;
+        }
+
+        public static string overtime_month(List<Day> listOfDays)
+        {
+            int working_day = 0;
+            int working_hours=0;
+            foreach (var day in listOfDays)
+            {
+                working_day++;
+            }
+            foreach (var day in listOfDays)
+            {
+                working_hours += day.hours;
+            }
+
+
+            return (working_day*8 < working_hours) ? "tak" : "nie";
+        }
+
+        private static string sunday_work(List<Day> listOfDays)
+        {
+            int working_sunday = 0;
+            foreach (var day in listOfDays)
+            {
+                if (day.week == 7 && day.hours != 0)
+                    return "tak";
+            }
+            return "nie";
+        }
+    }
+}
